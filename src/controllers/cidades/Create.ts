934386dc5c0f8ff3285ccba3f97d52798a9b185a
lucas mendes <lucasmndes.dev@gmail.com>
2025-04-23 15:18:23 -0300

    adicionando validação com yup

--- src/controllers/cidades/Create.ts
+++ src/controllers/cidades/Create.ts
@@ -1,18 +1,35 @@
 import { Request, Response } from "express"
 
+import * as yup from 'yup'
+
 interface ICidade{
   nome: string;
 }
 
+const bodyValidation: yup.ObjectSchema<ICidade>= yup.object().shape({
+  nome: yup.string().required().min(3)
+});
+
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export const create = (req: Request<{}, {}, ICidade>, res: Response)=>{
+export const create = async (req: Request<{}, {}, ICidade>, res: Response)=>{
 
-  const data: ICidade = req.body;
+  let validatedData: ICidade | undefined = undefined
+  // Validar o body da requisição
+  try{
+      validatedData = await bodyValidation.validate(req.body)
+  }catch(err){
+      const yupError= err as yup.ValidationError;
 
+      
+      res.json({
+        errors: {
+          default: yupError.message,
+        }
+      });
+  }
+  
 
-  console.log(data.nome);
+  console.log(validatedData);
 
-
-  res.send("Create Cidade");
 }

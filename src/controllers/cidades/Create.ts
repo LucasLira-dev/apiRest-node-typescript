7da96888a2da414894082af0b07be13b14a49c00
lucas mendes <lucasmndes.dev@gmail.com>
2025-04-25 14:39:13 -0300

    middleware para validação com yup

--- src/controllers/cidades/Create.ts
+++ src/controllers/cidades/Create.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express"
+import { Request, RequestHandler, Response } from "express"
 import { StatusCodes } from "http-status-codes";
 
 import * as yup from 'yup'
@@ -11,33 +11,35 @@ const bodyValidation: yup.ObjectSchema<ICidade>= yup.object().shape({
   nome: yup.string().required().min(3)
 });
 
+export const createBodyValidator: RequestHandler = async(req, res, next) =>{
+  try{
+    await bodyValidation.validate(req.body, {abortEarly: false});
+    return next(); // Se a validação passar, continue para o próximo middleware ou rota
+}catch(err){
+    const yupError= err as yup.ValidationError;
+
+
+    const validationErrors: Record<string, string> = {} // criando um objeto para armazenar os erros de validação
+    
+    yupError.inner.forEach((error) => {
+      if (!error.path) return;
+
+      validationErrors[error.path] = error.message;
+    }) //pegando os erros de validação e colocando em um objeto
+
+    
+    res.status(StatusCodes.BAD_REQUEST).json({
+      errors: validationErrors, // retornando os erros de validação
+    });
+}
+
+}
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export const create = async (req: Request<{}, {}, ICidade>, res: Response)=>{
 
-  let validatedData: ICidade | undefined = undefined
-  // Validar o body da requisição
-  try{
-      validatedData = await bodyValidation.validate(req.body, {abortEarly: false});
-  }catch(err){
-      const yupError= err as yup.ValidationError;
+ 
 
-
-      const validationErrors: Record<string, string> = {} // criando um objeto para armazenar os erros de validação
-      
-      yupError.inner.forEach((error) => {
-        if (!error.path) return;
-  
-        validationErrors[error.path] = error.message;
-      }) //pegando os erros de validação e colocando em um objeto
-
-      
-      res.status(StatusCodes.BAD_REQUEST).json({
-        errors: validationErrors, // retornando os erros de validação
-      });
-  }
-  
-
-  console.log(validatedData);
+  console.log(req.body);
 
 }
--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -10,7 +10,7 @@ router.get('/', (req, res) => {
   res.send('Eai!');
 });
 
-router.post('/cidades', CidadesController.create)
+router.post('/cidades', CidadesController.createBodyValidator, CidadesController.create)
 
 export default router;
 
